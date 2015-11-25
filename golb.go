68b1d257eb78a3d251f5e49081c9f7302b9d1161
Magnus Ahlberg <m@magnus.com>
2015-11-25 16:51:40 -0500

    Basic article support

--- golb.go
+++ golb.go
@@ -4,31 +4,84 @@ import (
 	"bytes"
 	"fmt"
 	"io/ioutil"
-	"os"
 	"net/http"
-	"github.com/knieriem/markdown"	
+	"os"
+	"path/filepath"
+	"strings"
+	"strconv"
+	"time"
+	"github.com/knieriem/markdown"
 )
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
+type Article struct {
+	name string
+	date time.Time
+	path string
+	html string
+}
+
+var (
+	articles []Article
+)
+
+func fetchArticles() {
+	walkErr := filepath.Walk("posts/", visit)
+	if walkErr != nil {
+		fmt.Printf("Walk error: %s", walkErr)
+	}
+}
+
+func visit(path string, info os.FileInfo, err error) error {
+	splitPath := strings.Split(path, "/")
+	if len(splitPath) > 4 {
+		name := splitPath[len(splitPath)-1]
+		name = name[:len(name)-len(".md")]
+		year, err := strconv.Atoi(splitPath[1])
+		month, err := strconv.Atoi(splitPath[2])
+		day, err := strconv.Atoi(splitPath[3])
+		if err != nil {
+			return nil //TODO: Error handling
+		}
+		date := time.Date(year, time.Month(month), day, 0, 0, 0, 0, time.UTC)
+
+		var buf bytes.Buffer
+		file, err := os.Open(path)
+		if err != nil {
+			fmt.Printf("Open error: %s", err)
+			return nil //TODO: Error handling
+		}
+		defer file.Close()
+		p := markdown.NewParser(nil)
+		p.Markdown(file, markdown.ToHTML(&buf))
+
+		article := Article{name, date, path, buf.String()}
+		articles = append(articles, article)
+		fmt.Printf("\nPath: %s\nDate: %s\nName: %s\nHTML:%s\n\n", path, date.String(), name, buf)
+	}
+	return nil
+}
+
+func renderPage(page bytes.Buffer, w http.ResponseWriter) {
 	header, _ := ioutil.ReadFile("templates/header.html")
 	footer, _ := ioutil.ReadFile("templates/footer.html")
-	var buf, page bytes.Buffer
-	file, err := os.Open("posts/index.md")
-	if err != nil {
-		return
+	fmt.Fprintf(w, "%s %s %s", header, page.String(), footer)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	var page bytes.Buffer
+	if len(articles) == 0 {
+		fetchArticles()
 	}
-	defer file.Close()
-	p := markdown.NewParser(nil)
-	p.Markdown(file, markdown.ToHTML(&buf))
-	page.Write(header)
-	page.Write(buf.Bytes())
-	page.Write(footer)
-	fmt.Fprintf(w, "%s", page.String())
+	for _, article := range articles {
+		fmt.Printf("Article: %s", article.name)
+		page.Write([]byte(article.html))
+	}
+	renderPage(page, w)
 }
 
 func main() {
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
 	http.HandleFunc("/", indexHandler)
-	http.ListenAndServe(":80", nil)
+	http.ListenAndServe(":8181", nil)
 }
 
